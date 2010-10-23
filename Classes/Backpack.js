53852423c169462d0549d043c7f02d1b543d33c4
JERE0012 <JERE0012@66a8ae2c-49f5-f242-93de-6cd216b7a5d7>
2010-10-23 22:55:36 +0000

    backpack now draws inventory on screen. also lets you remove items by pressing g
    git-svn-id: https://se347.student.main.ntu.edu.sg:8081/svn/FrenchToastMafia/trunk@16 66a8ae2c-49f5-f242-93de-6cd216b7a5d7

--- Classes/Backpack.js
+++ Classes/Backpack.js
@@ -1,24 +1,91 @@
 class Backpack extends MonoBehaviour {
 	var maxCapacity : int = 100;
 	var items = new Array();
+	var ui : Texture2D;
 
 	var activeItem : InventoryItem;
 
 	private var collideItems = new Array();
-		
+
 	// gui
+	function OnGUI() {
+		var activeHeight : float = 64.0;
+		var activeWidth : float = 64.0;
+		var normalHeight : float = 48.0;
+		var normalWidth : float = 48.0;
+		var gap : float = 1.0;
+		var screenLeftPadding : float = 10.0;
+		var screenBottomPadding : float = 10.0;
+				
+		if (items.length > 0) {
+			// do complex gui drawing
+
+			GUI.Box(new Rect(screenLeftPadding, Screen.height - screenBottomPadding - activeHeight, activeWidth, activeHeight), activeItem.worldName);
+			if (activeItem.consummable)
+				GUI.Label(new Rect(screenLeftPadding + 5, Screen.height - screenBottomPadding - 20.0, activeWidth - 5, 25.0), "" + activeItem.quantity);
+			
+			var i = 0;
+			
+			for (var item : InventoryItem in items) {
+				if (item !== activeItem) {
+					GUI.Box(new Rect((screenLeftPadding + activeWidth) + (i * normalWidth) + gap, Screen.height - screenBottomPadding - normalHeight, normalWidth, normalHeight), item.worldName);
+					if (item.consummable) {
+						GUI.Label(new Rect((screenLeftPadding + activeWidth) + (i * normalWidth) + gap + 5, Screen.height - screenBottomPadding - 20.0, normalWidth - 5, 25.0), "" + item.quantity);
+					}
+					i++;
+				}
+			}
+
+		} else {
+			GUI.Box(new Rect(screenLeftPadding, Screen.height - screenBottomPadding - activeHeight, activeWidth, activeHeight), "No Items");
+			
+		}
+		
+	}
 	
 	// detect objects
-	function FixedUpdate() {
-		if (Input.GetKey("e") && (collideItems.length > 0)) {
+	function Update() {
+		var idx;
+		
+		if (Input.GetKeyUp("e") && (collideItems.length > 0)) {
 			for (var item : InventoryItem in collideItems) {
 				if (addItem(item)) {
-					DestroyImmediate(item.gameObject);
+					Destroy(item.gameObject);
 				}
 			}
 			collideItems.clear();
+		} else if (Input.GetKeyUp("g") && (items.length > 0)) {
+			removeItem(activeItem);
 		}
-		Debug.Log(items.length);
+		
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+			// scroll down
+			Debug.Log("Scrolldown");
+			
+			idx = indexOfItem(activeItem, items);
+			if (idx == items.length) {
+				activeItem = items[0];	
+			} else {
+				activeItem = items[++idx];
+			}
+			
+			
+		} else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+			// scroll up
+			Debug.Log("Scrollup");
+			idx = indexOfItem(activeItem, items);
+			if (idx == 0) {
+				activeItem = items[items.length];	
+			} else {
+				activeItem = items[--idx];
+			}
+			
+		}
+
+		
+		if (items.length == 1) {
+			activeItem = items[0];	
+		}
 	}
 	
 	function notification(who : Object, msg : String, userInfo : Object) {
@@ -41,7 +108,7 @@ class Backpack extends MonoBehaviour {
 	
 	function indexOfItem(item : Object, items : Array) {
 		for (var i=0;i<items.length;i++) {
-			if (items[i] == item) {
+			if (items[i] === item) {
 				return i;	
 			}	
 		}
@@ -70,7 +137,5 @@ class Backpack extends MonoBehaviour {
 	function removeItemAtIndex(idx : int) {
 		items.RemoveAt(idx);	
 	}
-	
-
 
 }
