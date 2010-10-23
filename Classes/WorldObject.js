b6dbd5976cc1e86a1bf8c6b6370c977be8dc0166
JERE0012 <JERE0012@66a8ae2c-49f5-f242-93de-6cd216b7a5d7>
2010-10-23 21:32:37 +0000

    added common worldobject functions
    git-svn-id: https://se347.student.main.ntu.edu.sg:8081/svn/FrenchToastMafia/trunk@13 66a8ae2c-49f5-f242-93de-6cd216b7a5d7

--- Classes/WorldObject.js
+++ Classes/WorldObject.js
@@ -1,4 +1,18 @@
 class WorldObject extends MonoBehaviour {
 	
 	var worldName : String = "";	
+	
+	function notify(who : Object, msg : String, userInfo : Object) {
+		// target must respond to notification
+		who.notification(this, msg, userInfo);
+	}
+	
+	function indexOfItem(item : Object, items : Array) {
+		for (var i=0;i<items.length;i++) {
+			if (items[i] == item) {
+				return i;
+			}	
+		}
+		return -1;
+	}
 }
