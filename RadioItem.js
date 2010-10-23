2363708ee1b6de3ae448b463f15e9e9c5cf8aba4
JERE0012 <JERE0012@66a8ae2c-49f5-f242-93de-6cd216b7a5d7>
2010-10-23 21:32:39 +0000

    sets radio item properties
    git-svn-id: https://se347.student.main.ntu.edu.sg:8081/svn/FrenchToastMafia/trunk@14 66a8ae2c-49f5-f242-93de-6cd216b7a5d7

--- RadioItem.js
+++ RadioItem.js
@@ -1,5 +1,12 @@
 class RadioItem extends InventoryItem {
-	
+
+	function Start() {
+		worldName  = "Amateur Radio";
+		description = "Push to talk functionality for sending and receiving of radio communications. Has the ability to send morse code as well.";
+		quantity = 1;
+		unitSpaceRequired = 1;
+	}
+
 	function Update() {
 		
 	}
