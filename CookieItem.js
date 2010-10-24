fc1c9ac262ef980b905baccc97f8f208feb177d6
JERE0012 <JERE0012@66a8ae2c-49f5-f242-93de-6cd216b7a5d7>
2010-10-24 11:36:48 +0000

    cookies give you health too
    git-svn-id: https://se347.student.main.ntu.edu.sg:8081/svn/FrenchToastMafia/trunk@23 66a8ae2c-49f5-f242-93de-6cd216b7a5d7

---
+++ CookieItem.js
@@ -0,0 +1,34 @@
+class CookieItem extends InventoryItem {
+
+	function Start() {
+		worldName  = "Box of cookies";
+		description = "Girl scout mint chocolate chip cookies. Mmmmmmmmmmmm.";
+		quantity = 20;
+		unitSpaceRequired = 1;
+		consummable = true;
+		prefabName = "Cookie";
+	}
+
+	function performFunction() {
+		quantity--;
+		GameObject.FindWithTag("GameController").GetComponent(Player).adjustHealth(5);
+	}
+	
+	function activeWillUpdateFunction() {
+	}
+	function activeDidUpdateFunction() {
+	}
+	function activeWillGUIFunction() {
+	}
+	function activeDidGUIFunction() {
+	}
+	function equippedWillUpdateFunction() {
+	}
+	function equippedDidUpdateFunction() {
+	}
+	function equippedWillGUIFunction() {
+	}
+	function equippedDidGUIFunction() {
+	}
+
+}
