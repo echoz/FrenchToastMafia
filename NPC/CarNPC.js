23cb2ddd15d5032a6f79d3cb0c82e57b925589f9
Jeremy Foo <jeremy@wellhello.(none)>
2010-11-07 17:51:09 +0800

    NPC scripts

---
+++ NPC/CarNPC.js
@@ -0,0 +1,151 @@
+class CarNPC extends NPC {
+	//Credit to Andrew Gotow 2009 for the car script 
+	// These variables allow the script to power the wheels of the car.
+	var FrontLeftWheel : WheelCollider;
+	var FrontRightWheel : WheelCollider;
+	var GearRatio : float[];
+	var CurrentGear : int = 0;
+	// These variables are just for applying torque to the wheels
+	var EngineTorque : float = 600.0;
+	var MaxEngineRPM : float = 3000.0;
+	var MinEngineRPM : float = 1000.0;
+	private var EngineRPM : float = 0.0;
+	//The petrol the car has
+	var maxPetrol : int = 500;
+	var curPetrol : float = 500;
+	//To toggle headlights
+	var carLightL : Light;
+	var carLightR : Light;
+	var warning : Texture2D;
+	var nopetrol : Texture2D;
+	public var fadeSpeed = 0.3;
+	var drawDepth = -1000;
+	private var alpha = 1.0;
+	private var fadeDir = -1;
+	
+	function Awake () 
+	{
+		// I usually alter the center of mass to make the car more stable. I'ts less likely to flip this way.
+		rigidbody.centerOfMass.y = -2.5;
+	}
+	
+	/*
+		Instructions on entering car
+		new Subtitle("Press [W] or [S] to move forward or reverse.", 3);
+		new Subtitle("Press [A] or [D] to turn left or right.", 3);
+		new Subtitle("Press [SpaceBar] to brake.", 3);
+		new Subtitle("Press [G] to get out the car.", 3);
+		new Subtitle("Press [F] to on/off light.", 3);
+		
+	*/
+	
+	//Draw the amount of petrol left
+	function OnGUI()
+	{
+		GUI.Label(new Rect(Screen.width/2*0.68 , 15, maxPetrol, 20),"Petrol: ");
+		GUI.Box(new Rect(Screen.width/2 - maxPetrol/2, 15, maxPetrol, 20),"");
+		//if petrol is empty juz show a empty bar
+		if(curPetrol > 0)
+		{
+			GUI.Box(new Rect(Screen.width/2 - maxPetrol/2, 15, curPetrol, 20),"");
+		}
+		//Give a warning when petrol is less than 35%
+		if(curPetrol > 0 && curPetrol < maxPetrol*0.35)
+		{
+			alpha += fadeDir * fadeSpeed * Time.deltaTime; 
+			alpha = Mathf.Clamp01(alpha);   
+			GUI.color.a = alpha;
+			GUI.depth = drawDepth;  
+			GUI.DrawTexture(new Rect(Screen.width/2 - warning.width/4, 55, warning.width/2, warning.height/2),warning);
+			//GUI.Label(new Rect(Screen.width/2 - warning.width/4, 55, warning.width/2, warning.height/2),warning);
+		}
+		//Warning on no petrol
+		if(curPetrol <= 0)
+		{
+			GUI.Label(new Rect(Screen.width/2 - nopetrol.width/4, 55, nopetrol.width/2, nopetrol.height/2),nopetrol);
+		}
+	}
+
+	//The movement of the car
+	function Update () 
+	{
+		// This is to limith the maximum speed of the car, adjusting the drag probably isn't the best way of doing it,
+		// but it's easy, and it doesn't interfere with the physics processing.
+		rigidbody.drag = rigidbody.velocity.magnitude / 250;
+		
+		//I HAVE NO IDEA HOW THIS SHIT WORKS. but i added brake and only move when player press "w" or "s" and consume petrol
+		//press spacebar to brake
+		if(Input.GetButton("Jump") || curPetrol <= 0)
+		{
+			FrontLeftWheel.brakeTorque = 1000;
+			FrontRightWheel.brakeTorque = 1000;
+		}
+		else
+		{	
+			FrontLeftWheel.brakeTorque = 0;
+			FrontRightWheel.brakeTorque = 0;
+			// finally, apply the values to the wheels.	The torque applied is divided by the current gear, and
+			// multiplied by the user input variable.
+			//"w" to move forward and "s" to move backward
+			if(Input.GetKey("w") || Input.GetKey("s"))
+			{
+				EngineRPM = (FrontLeftWheel.rpm + FrontRightWheel.rpm)/2 * GearRatio[CurrentGear];
+				ShiftGears();
+				FrontLeftWheel.motorTorque = EngineTorque / GearRatio[CurrentGear] * Input.GetAxis("Vertical");
+				FrontRightWheel.motorTorque = EngineTorque / GearRatio[CurrentGear] * Input.GetAxis("Vertical");
+				curPetrol -= 0.05;
+			}
+			//Don't move then not pressing "w" or "s"
+			else
+			{
+				FrontLeftWheel.brakeTorque = 300;
+				FrontRightWheel.brakeTorque = 300;
+			}
+		}
+		// the steer angle is an arbitrary value multiplied by the user input.
+		FrontLeftWheel.steerAngle = 15 * Input.GetAxis("Horizontal");
+		FrontRightWheel.steerAngle = 15 * Input.GetAxis("Horizontal");
+		//toggle headlights
+		if(Input.GetKeyUp("f"))
+		{
+			carLightL.enabled = !carLightL.enabled;
+			carLightR.enabled = !carLightR.enabled;
+		}
+		//this area is to set the gui to fade in and out
+		if(alpha == 1.0){
+			fadeDir = -1;  
+		}
+		if(alpha == 0.0){
+			fadeDir = 1;  
+		}
+	}
+	function ShiftGears() {
+		// this funciton shifts the gears of the vehcile, it loops through all the gears, checking which will make
+		// the engine RPM fall within the desired range. The gear is then set to this "appropriate" value.
+		if ( EngineRPM >= MaxEngineRPM ) {
+			var AppropriateGear : int = CurrentGear;
+			
+			for ( var i = 0; i < GearRatio.length; i ++ ) {
+				if ( FrontLeftWheel.rpm * GearRatio[i] < MaxEngineRPM ) {
+					AppropriateGear = i;
+					break;
+				}
+			}
+			
+			CurrentGear = AppropriateGear;
+		}
+		
+		if ( EngineRPM <= MinEngineRPM ) {
+			AppropriateGear = CurrentGear;
+			
+			for ( var j = GearRatio.length-1; j >= 0; j -- ) {
+				if ( FrontLeftWheel.rpm * GearRatio[j] > MinEngineRPM ) {
+					AppropriateGear = j;
+					break;
+				}
+			}
+			
+			CurrentGear = AppropriateGear;
+		}
+	}	
+}
---
+++ NPC/RadioNPC.js
@@ -0,0 +1,156 @@
+class RadioNPC extends NPC {
+	
+	var screen : GameObject;
+	var led : GameObject;
+
+	var freqText : GameObject;
+	var freqTextTextStyle = new GUIStyle();
+	var ptt_map_image : Texture;
+	var pttTextStyle : GUIStyle;	
+	
+	var instructions : GameObject;
+	var instructionsLetterPause : float = 0.02;
+	var instructionsSound : AudioClip;
+	var instructionsMorse : Texture2D;
+	var instructionsTextStyle = new GUIStyle();
+	
+	private var on : boolean = false;
+	private var PTTENABLED : boolean = false;
+	private var FREQ: float = 0.0;
+	private var CODE = "";
+	private var CORRECT_FREQ : float = 12.50;
+	private var CORRECT_CODE = "...---...";
+
+	function TypeText(word : String) {
+		for (var letter in word.ToCharArray()) {
+			instructions.guiText.text += letter;
+			if (instructionsSound) {
+				instructions.audio.PlayOneShot(instructionsSound);	
+			}
+			yield WaitForSeconds(instructionsLetterPause);
+		}
+		instructions.audio.Stop();
+	}
+
+	function Awake() {
+		on = false;
+		var word = instructions.guiText.text;
+		instructions.guiText.text = "";
+		TypeText(word);
+	}
+
+	function OnGUI() {
+		
+		// update frequency screen
+		if (on) {
+			if (FREQ == CORRECT_FREQ) {
+				freqTextTextStyle.normal.textColor = Color.green;
+			} else {
+				freqTextTextStyle.normal.textColor = Color.red;				
+			}	
+			GUI.Label(new Rect(Screen.width - 740, Screen.height - 400, 350, 300), freqText.guiText.text, freqTextTextStyle);
+		}	
+		
+		//display the instructions and the morse code image
+		GUI.Label(new Rect(Screen.width - 510, Screen.height - 600, 490,600), instructions.guiText.text, instructionsTextStyle);
+		GUI.Box(new Rect(10, 10, 300, 450),instructionsMorse);
+		
+			
+		if (!PTTENABLED) {	
+			// display correct code stuff
+			var windowRect : Rect = new Rect(Screen.width/2 - 400, Screen.height/2 - 300, 800, 500);
+			if (CODE == CORRECT_CODE) {
+				GUI.Window(0,windowRect, WindowFunction, "");
+			}
+		}
+	}
+
+	function WindowFunction(windowID : int) {
+		//map image is displayed in the pop-up window.
+		GUI.Label(new Rect(120, 15, 540, 400), ptt_map_image);
+		GUI.Label(new Rect(120, 350, 540, 100), "Proceed to the location marked on the map.", pttTextStyle);
+		//when the exit button is pressed, the window is destroyed.
+		if (GUI.Button (Rect (120, 400, 540, 50), "Exit", "button")) 
+		{
+			Destroy (this);
+			
+			var director = GameObject.FindWithTag("god").GetComponent(Director);
+			director.globalState.Add("sosSent", true);
+			director.previous_level();
+
+		}
+
+	}
+
+	function Update() {
+		
+		// update frequency display
+		freqText.guiText.text = "" + FREQ.ToString("00.00");
+		
+		// set screen and led color
+		if (on) {
+			screen.renderer.material.color = Color.gray;
+			if (PTTENABLED) {
+				led.renderer.material.color = Color.red + Color.yellow;
+			} else {
+				led.renderer.material.color = Color.green;
+			}
+		} else {
+			screen.renderer.material.color = Color.black;
+			PTTENABLED = false;
+			led.renderer.material.color = Color.red;
+		}
+
+		// reset code if PPT is not enabled		
+		if (!PTTENABLED) {
+			if (CODE != CORRECT_CODE) {
+				CODE = "";	
+			}	
+		}
+		
+		if (Input.GetMouseButtonDown(0)) {
+		
+			var hit : RaycastHit;
+			var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			if (Physics.Raycast(ray, hit) && hit.collider.name == "off_btn") {
+				if (on) {
+					on = false;
+					FREQ = 0.0;
+					var director = GameObject.FindWithTag("god").GetComponent(Director);
+					director.previous_level();
+						
+				}
+			} else if (Physics.Raycast(ray, hit) && hit.collider.name == "on_btn") {
+				if (!on)
+					on = true;	
+			} else if (Physics.Raycast(ray, hit) && hit.collider.name == "PTT") {
+								
+				if (FREQ == CORRECT_FREQ) {
+					PTTENABLED = !PTTENABLED;
+				} 
+			} else if (Physics.Raycast(ray, hit) && hit.collider.name == "Long_Btn") {
+				if (PTTENABLED) {
+					CODE = CODE + "-";	
+				}
+			} else if (Physics.Raycast(ray, hit) && hit.collider.name == "Short_Btn") {
+				if (PTTENABLED) {
+					CODE = CODE + ".";	
+				}
+			} else if (Physics.Raycast(ray, hit) && hit.collider.name == "Down" && !PTTENABLED) {
+				if (on) {
+					if (FREQ == 0.5) {
+						FREQ = 0;
+					} else {
+						FREQ -= 0.5;
+					}
+				}	
+			} else if (Physics.Raycast(ray, hit) && hit.collider.name == "Up" && !PTTENABLED) {
+				if (on) {
+					FREQ += 0.5;	
+				}	
+			}
+		}
+	}
+	
+}
