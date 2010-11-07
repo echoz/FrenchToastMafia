7156c5e2db91fb6ac723c254376a12e4c2a2ce90
Jeremy Foo <jeremy@wellhello.(none)>
2010-11-07 13:45:14 +0800

    new items!

---
+++ UmbrellaItem.js
@@ -0,0 +1,35 @@
+class UmbrellaItem extends InventoryItem {
+
+	function Awake() {
+		worldName  = "Umbrella";
+		description = "I'm made of sugar!";
+		quantity = 1;
+		unitSpaceRequired = 1;
+		consummable = false;
+		prefabName = "Umbrella";
+	}
+
+	function activeWillUpdateFunction() {
+	}
+	function activeDidUpdateFunction() {
+	}
+	function activeWillGUIFunction() {
+	}
+	function activeDidGUIFunction() {
+	}
+	function equippedWillUpdateFunction() {
+	}
+	function equippedDidUpdateFunction() {
+	}
+	function equippedWillGUIFunction() {
+	}
+	function equippedDidGUIFunction() {
+	}
+	function performFunction() {
+	}
+	function willThrowItem() {
+	}
+	function didThrowItem() {
+	}
+		
+}
