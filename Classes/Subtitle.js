1538074af0c0078ea89ec0191e5e398329244ec5
JERE0012 <JERE0012@66a8ae2c-49f5-f242-93de-6cd216b7a5d7>
2010-11-17 18:58:53 +0000

    subtitles now have a postdelay
    git-svn-id: https://se347.student.main.ntu.edu.sg:8081/svn/FrenchToastMafia/trunk@66 66a8ae2c-49f5-f242-93de-6cd216b7a5d7

--- Classes/Subtitle.js
+++ Classes/Subtitle.js
@@ -1,10 +1,18 @@
 class Subtitle { 
 	public var content : String;
 	public var displayTime : float;
+	public var postDelay : float;
+
+	public function Subtitle(content : String, displayTime : float, postDelay : float) {
+		this.content = content;
+		this.displayTime = displayTime;	
+		this.postDelay = postDelay;
+	}
 	
 	public function Subtitle(content : String, displayTime : float) {
 		this.content = content;
 		this.displayTime = displayTime;	
+		this.postDelay = 0;
 	}	
 	
 }
