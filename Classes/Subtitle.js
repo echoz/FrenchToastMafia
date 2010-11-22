7ab7206552c8b0e3ceb78c6cc804163ba10acc53
JERE0012 <JERE0012@66a8ae2c-49f5-f242-93de-6cd216b7a5d7>
2010-11-22 16:56:50 +0000

    subtitle system that adds callbacks for when a subtitle is displayed
    git-svn-id: https://se347.student.main.ntu.edu.sg:8081/svn/FrenchToastMafia/trunk@85 66a8ae2c-49f5-f242-93de-6cd216b7a5d7

--- Classes/Subtitle.js
+++ Classes/Subtitle.js
@@ -1,7 +1,17 @@
 class Subtitle { 
 	public var content : String;
-	public var displayTime : float;
-	public var postDelay : float;
+	public var displayTime : float = 0;
+	public var postDelay : float = 0;
+	public var callbackDelegate = null;
+	public var callbackMessage : String = "";
+
+	public function Subtitle(content : String, displayTime : float, postDelay : float, delegate, delegateMsg : String) {
+		this.callbackDelegate = delegate;
+		this.callbackMessage = delegateMsg;
+		this.content = content;
+		this.displayTime = displayTime;	
+		this.postDelay = postDelay;		
+	}
 
 	public function Subtitle(content : String, displayTime : float, postDelay : float) {
 		this.content = content;
@@ -12,7 +22,6 @@ class Subtitle {
 	public function Subtitle(content : String, displayTime : float) {
 		this.content = content;
 		this.displayTime = displayTime;	
-		this.postDelay = 0;
 	}	
 	
 }
