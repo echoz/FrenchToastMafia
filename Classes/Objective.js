b608d54931e1699fa3a5768f0aa010ce752081de
Jeremy <jeremyfoo@gmail.com>
2010-11-24 16:24:15 +0800

    objectives now implements event handling for restoring and saving of states by director

--- Classes/Objective.js
+++ Classes/Objective.js
@@ -2,6 +2,7 @@ class Objective extends MonoBehaviour {
 	public var completed : boolean = false;
 	var nextLevel : String;
 	var developer : boolean = false;
+	var className : String;
 
 	var subtitleDelay : float = 2;
 	protected var subtitlesDone : boolean = false;
@@ -29,4 +30,16 @@ class Objective extends MonoBehaviour {
 		Debug.Log("Please override canLoadLevel");
 		return false;
 	}
+	
+	function willSaveState() {
+	}
+
+	function didSaveState() {
+	}
+	
+	function willRestoreState() {
+	}
+	
+	function didRestoreState() {
+	}	
 }
